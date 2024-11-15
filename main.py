06ec34d72a07b41c17e9c8ef58a96ea8e13f02c4
danjpilk <PIL23210822@student.runshaw.ac.uk>
2024-11-15 15:03:45 +0000

    Created Person Class

--- main.py
+++ main.py
@@ -22,11 +22,40 @@ class Person():
     def setPersonStartLocation(self, personStartLocation):
         self.personStartLocation = personStartLocation
     
+    def getPersonGoldCoins(self):
+        return self.personGoldCoins
+    
+    def setPersonGoldCoins(self, personGoldCoins):
+        self.personGoldCoins = personGoldCoins
+    
     def getPersonProfession(self):
         return self.personProfession
     
     def setPersonProfession(self, personProfession):
-        
+        self.personProfession = personProfession
+
+    def getPersonSpeed(self):
+        return self.personSpeed
+    
+    def setPersonSpeed(self, personSpeed):
+        self.personSpeed = personSpeed
+    
+    def getPersonHealthPoints(self):
+        return self.personHealthPoints
+    
+    def setPersonHealthPoints(self, personHealthPoints):
+        self.personHealthPoints = personHealthPoints
+
+    def personTalk(dialogueLine):
+        return f"{dialogueLine}"
+
+    def personWalk(self, direction, distance):
+        return f"{self.personName} walked in the {direction} for {distance} units"
+
+    def personAttacked(self, healthPoints, Attacker):
+        self.personHealthPoints -= healthPoints
+        return f"{self.personName} lost {healthPoints} by {Attacker}"
+
 
 #Character Classes
 
