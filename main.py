e78fae2d050f62e8e4a7312df7e056f77e7fea2d
Daniel <PIL23210822@student.runshaw.ac.uk>
2024-11-16 12:53:31 +0000

    Added monsters

--- main.py
+++ main.py
@@ -5,8 +5,6 @@
 #Allow for some form of attacking back which requires skill
 #Levels with different levels of enemies of different difficulty
 
-
-
 #Main Classes 
 class Person(): #Person Class SuperClass of all Person Classes (Paladin, Barbarian, Conjurer, Sorcerer etc)
     def __init__(self, personName, personAge, personStartLocation, personGoldCoins, 
@@ -108,9 +106,169 @@ class Warrior(Person):
     def WarriorTalk(self, dialogueLineWarrior):
         return f"{dialogueLineWarrior}"
     
-    def WarriorFight(self, WarriorCombatSkill, WarriorWeapon, CurrentEnemy):
+    def WarriorFight(self, WarriorCombatSkill, WarriorWeapon, currentEnemy):
         currentHP = self.WarriorHealthPoints
+        
         
 
         
 #Character SubClasses
+
+
+
+
+#Enemy SubClasses
+class Monster:
+    def __init__(self, monsterName, monsterHealthPoints, monsterDialogueLine):
+        self.monsterName = monsterName
+        self.monsterHealthPoints = monsterHealthPoints
+        self.monsterDialogueLine = monsterDialogueLine
+    
+    def setMonsterName(self, monsterName):
+        self.monsterName = monsterName
+    
+    def getMonsterName(self):
+        return self.monsterName
+        
+    def setMonsterHealthPoints(self, monsterHealthPoints):
+        self.monsterHealthPoints = monsterHealthPoints
+    
+    def getMonsterHealthPoints(self):
+        return self.monsterHealthPoints
+    
+    def setMonsterDialogueLine(self, monsterDialogueLine):
+        self.monsterDialogueLine = monsterDialogueLine
+    
+    def getMonsterDialogueLine(self): #Speak function as well
+        return self.monsterDialogueLine
+    
+
+    def take_Damage(self, damage):
+        if self.monsterHealthPoints > 0:
+            self.monsterHealthPoints -= damage
+            return f"the monsters health points are now {self.monsterHealthPoints}"
+            
+        else:
+            return f"{self.monsterName} has reached 0 health points"
+            
+            
+            
+class Friend(Monster): #SubClass of Monster
+    def __init__(self, monsterName, monsterHealthPoints, monsterDialogueLine, highFiveDialogue, monsterGift): #constructor method
+        super().__init__(monsterName, monsterHealthPoints, monsterDialogueLine) #super constructor
+        self.highFiveDialogue = highFiveDialogue #initalises highFiveDialogue
+        self.monsterGift = monsterGift # Same as Line 39 but monsterGift
+        
+        #getters and setters
+        
+    def getHighFive(self):
+        return self.highFiveDialogue
+    
+    def setHighFive(self, high5):
+        self.highFiveDialogue = high5
+        
+    def getMonsterGift(self):
+        return self.monsterGift
+    
+    def setMonsterGift(self, gift):
+        self.monsterGift = gift
+    
+    #highFive function
+    def highFive(self):
+        print(f"{self.name} says {self.highFiveDialogue}!")
+        return f"You have received a Gift from {self.name}"
+        
+        
+
+
+class Enemy(Monster):
+    def __init__(self, name, healthPoints, dialogueLine, damage, weakness):
+        super().__init__(name, healthPoints, dialogueLine)
+        self.damage = damage
+        self.weakness = weakness
+    
+    def getDamage(self):
+        return self.damage
+    
+    def setDamage(self, damage):
+        self.damage = damage
+        
+    def getWeakness(self):
+        return self.weakness
+    
+    def setWeakness(self, weakness):
+        self.weakness = weakness
+        
+    
+    def enemyFight(self, weakness):
+        print(f"{self.dialogueLine}")
+        weapon = input("Choose a weapon\n")
+        if weapon.lower() == weakness.lower():
+            print("Enemy Defeated!")
+            print( f"Player Wins!")
+            return True
+        
+        else:
+            print(f"Player Loses!")
+            return False
+        
+#----------------------------------------------------------       
+#Player    
+#----------------------------------------------------------
+#class Player():
+    def __init__(self, name, healthPoints, playerClassType):
+        self.name = name
+        self.healthPoints = healthPoints
+        self.playerClassType = playerClassType
+    
+    
+    def getName(self):
+        return self.name
+    
+    def setName(self, name):
+        self.name = name
+        
+    def getHealthPoints(self):
+        return self.healthPoints
+
+    def setHealthPoints(self, healthPoints):
+        self.healthPoints = healthPoints
+    
+    def getPlayerClassType(self):
+        return playerClassType
+    
+    def setPlayerClassType(self, playerClassType):
+        self.playerClassType = playerClassType
+        
+
+            
+  
+#----------------------------------------------------------
+#Game
+#----------------------------------------------------------
+#def encounterEnemyMonster():
+    monsterNames = ["Bob", "Brian", "Dave"]
+    dialogueLines = ["BRAINSSSS", "BRAINSS", "BRAINSSS"]
+    weaknessList = ["Cats", "Dogs", "Cheese"]
+    import random
+    randName = random.randint(0, len(monsterNames)-1)
+    randHealthPoints = random.randint(100, 200)
+    randDialogueLines = random.randint(0, len(dialogueLines)-1)
+    randDamage = random.randint(1, 6)
+    randWeakness = random.randint(0, len(weaknessList)-1)
+    zombie = Enemy(monsterNames[randName], randHealthPoints, dialogueLines[randDialogueLines], randDamage, weaknessList[randWeakness])
+    print(zombie.enemyFight(zombie.weakness))
+    if zombie.enemyFight() == True:
+        encounterFriend()
+    else:
+        print("Game Over")
+
+#----------------------------------------------------------
+#zombie = Monster("Zombie", 100, "BRAINS")
+#friend1 = Friend("Bob", 200, "Hey!", "abc", "£20")
+#print(friend1.highFive())
+#enemy1 = Enemy("Bob", 200, "Hey!", 5, "£20")
+#print(enemy1.enemyFight(enemy1.weakness))
+#encounterEnemyMonster()
+    
+#----------------------------------------------------------    
